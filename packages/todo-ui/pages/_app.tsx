af7ef2f8bebc0f329ae4bef7759ddecfe5b8cce0
Yoshitaka Terazawa <locol6622@gmail.com>
2020-07-24 17:52:53 +0900

    feat: change preact to react

--- packages/todo-ui/pages/_app.tsx
+++ packages/todo-ui/pages/_app.tsx
@@ -1,8 +1,11 @@
-import { h } from 'preact'
+import React from 'react'
 import { setup } from 'goober'
 import { prefix } from 'goober-autoprefixer'
+import { AppPropsType } from 'next/dist/next-server/lib/utils'
 
-setup(h, prefix)
+setup(React.createElement, prefix)
 
-export default ({ Component, pageProps }) => <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppPropsType) => <Component {...pageProps} />
+
+export default App
 
