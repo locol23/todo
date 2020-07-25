cc29f49914514c44bed79429f4a7878339759b40
Yoshitaka Terazawa <locol6622@gmail.com>
2020-07-25 22:24:55 +0900

    refactor: move title tag

--- packages/todo-ui/pages/_app.tsx
+++ packages/todo-ui/pages/_app.tsx
@@ -2,10 +2,17 @@ import React from 'react'
 import { setup } from 'goober'
 import { prefix } from 'goober-autoprefixer'
 import { AppPropsType } from 'next/dist/next-server/lib/utils'
+import Head from 'next/head'
 
 setup(React.createElement, prefix)
 
-const App = ({ Component, pageProps }: AppPropsType) => <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppPropsType) => (
+  <>
+    <Head>
+      <title>todone</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
+)
 
 export default App
-
